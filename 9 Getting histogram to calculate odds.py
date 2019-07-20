3902c15496b902b02a8693813a98f8a598b6c361
Abhinav <abhinav2scientist@gmail.com>
2019-07-20 19:42:56 +0530

    plot of 500 data

--- 9 Getting histogram to calculate odds.py
+++ 9 Getting histogram to calculate odds.py
@@ -27,6 +27,6 @@ np_aw_t = np.transpose(np.array(all_walks))
 # Select last row from np_aw_t: ends
 ends = np_aw_t[-1]  # last sublist that has last step at end of 100 throws
 
-# Plot histogram of ends, display plot
+# Plot histogram of ends, display plot (plot of 500 elements)
 plt.hist(ends)
 plt.show()
